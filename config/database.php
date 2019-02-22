0048282c46951e5176de755cd204df54411f2354
silver29 <becquerk29@gmail.com>
2019-02-21 21:31:27 -0500

    update database

--- config/database.php
+++ config/database.php
@@ -54,15 +54,15 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
+            'host' => env('DB_HOST', 'db4free.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'bloglaravel2019'),
+            'username' => env('DB_USERNAME', 'beck2019'),
+            'password' => env('DB_PASSWORD', 'beck2019@'),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
-            'strict' => false,
+            'strict' => true,
             'engine' => null,
         ],
 
